76b18cef9644fa88b687f45427bdca59925fc5cb
AhoiJ <j.aho@luukku.com>
2021-11-30 15:58:31 +0200

    Simple unit tests for isObjectlike, installed coveralls

--- test/isObjectLike.test.js
+++ test/isObjectLike.test.js
@@ -4,15 +4,21 @@ import chai from "chai"
 //var chai = require("chai")
 const expect = chai.expect
 
-//var should = require('chai').should()
-
-
 
 describe("isObjectLike", () => {
 
-    it("Integer", ()=>{
+    it("Check integer, false", ()=>{
         expect(isObject(5)).to.equal(false)
     });
+    it("Check string, false", () => {
+        expect(isObject("a")).to.equal(false)
+    });
+    it("Check empty, false", () => {
+        expect(isObject()).to.equal(false)
+    });
+    it("Check with object, true", () => {
+        expect(isObject([1,2,3])).to.equal(true)
+    })
 
 });
 
